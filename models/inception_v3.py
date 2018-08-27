74c2ac8df59f4ffea0689bf53626dab8a32c343a
Tianz <ssstz@outlook.com>
2018-08-27 17:19:21 +0800

    fix bugs

--- models/inception_v3.py
+++ models/inception_v3.py
@@ -7,7 +7,7 @@ from utils.keras_wrapped_layers import drop_connect_conv_2d, drop_connect_dense
 
 def conv2d_factory(x, nb_filters, kernel_size=(3, 3), drop_prob=0.5,
                  is_training=True, weight_decay=1E-4):
-    drop_connect_conv_2d(x, nb_filters, kernel_size,
+    x = drop_connect_conv_2d(x, nb_filters, kernel_size,
                          kernel_initializer='he_normal',
                          padding="same",
                          use_bias=False,
@@ -29,17 +29,17 @@ def inception_v3(nb_classes, is_training=True):
 
     # stage 1 32x32x64
     for _ in range(2):
-        branch1x1 = conv2d_factory(x, 16, (1, 1), is_training=is_training)
+        branch1x1 = conv2d_factory(x, 32, (1, 1), is_training=is_training)
 
-        branch5x5 = conv2d_factory(x, 16, (1, 1), is_training=is_training)
+        branch5x5 = conv2d_factory(x, 32, (1, 1), is_training=is_training)
         branch5x5 = conv2d_factory(branch5x5, 16, (5, 5), is_training=is_training)
 
-        branch3x3 = conv2d_factory(x, 16, (1, 1), is_training=is_training)
-        branch3x3 = conv2d_factory(branch3x3, 16, (3, 3), is_training=is_training)
-        branch3x3 = conv2d_factory(branch3x3, 16, (3, 3), is_training=is_training)
+        branch3x3 = conv2d_factory(x, 32, (1, 1), is_training=is_training)
+        branch3x3 = conv2d_factory(branch3x3, 32, (3, 3), is_training=is_training)
+        branch3x3 = conv2d_factory(branch3x3, 32, (3, 3), is_training=is_training)
 
         branch_pool = AveragePooling2D(pool_size=(2, 2), strides=(1, 1), padding='same')(x)
-        branch_pool = conv2d_factory(branch_pool, 16, (1, 1), is_training=is_training)
+        branch_pool = conv2d_factory(branch_pool, 32, (1, 1), is_training=is_training)
 
         x = concatenate([branch1x1, branch5x5, branch3x3, branch_pool], axis=-1)
 
@@ -47,20 +47,20 @@ def inception_v3(nb_classes, is_training=True):
 
     # stage 2 16x16x128
     for _ in range(2):
-        branch1x1 = conv2d_factory(x, 32, (1, 1), is_training=is_training)
+        branch1x1 = conv2d_factory(x, 64, (1, 1), is_training=is_training)
 
-        branch5x5 = conv2d_factory(x, 32, (1, 1), is_training=is_training)
-        branch5x5 = conv2d_factory(branch5x5, 32, (5, 1), is_training=is_training)
-        branch5x5 = conv2d_factory(branch5x5, 32, (1, 5), is_training=is_training)
+        branch5x5 = conv2d_factory(x, 64, (1, 1), is_training=is_training)
+        branch5x5 = conv2d_factory(branch5x5, 64, (5, 1), is_training=is_training)
+        branch5x5 = conv2d_factory(branch5x5, 64, (1, 5), is_training=is_training)
 
-        branch7x7 = conv2d_factory(x, 32, (1, 1), is_training=is_training)
-        branch7x7 = conv2d_factory(branch7x7, 32, (7, 1), is_training=is_training)
-        branch7x7 = conv2d_factory(branch7x7, 32, (1, 7), is_training=is_training)
-        branch7x7 = conv2d_factory(branch7x7, 32, (7, 1), is_training=is_training)
-        branch7x7 = conv2d_factory(branch7x7, 32, (1, 7), is_training=is_training)
+        branch7x7 = conv2d_factory(x, 64, (1, 1), is_training=is_training)
+        branch7x7 = conv2d_factory(branch7x7, 64, (7, 1), is_training=is_training)
+        branch7x7 = conv2d_factory(branch7x7, 64, (1, 7), is_training=is_training)
+        branch7x7 = conv2d_factory(branch7x7, 64, (7, 1), is_training=is_training)
+        branch7x7 = conv2d_factory(branch7x7, 64, (1, 7), is_training=is_training)
 
         branch_pool = AveragePooling2D(pool_size=(2, 2), strides=(1, 1), padding='same')(x)
-        branch_pool = conv2d_factory(branch_pool, 32, (1, 1), is_training=is_training)
+        branch_pool = conv2d_factory(branch_pool, 64, (1, 1), is_training=is_training)
 
         x = concatenate([branch1x1, branch5x5, branch7x7, branch_pool], axis=-1)
 
@@ -68,21 +68,21 @@ def inception_v3(nb_classes, is_training=True):
 
     # stage 3 8x8x256
     for _ in range(2):
-        branch1x1 = conv2d_factory(x, 64, (1, 1), is_training=is_training)
+        branch1x1 = conv2d_factory(x, 128, (1, 1), is_training=is_training)
 
-        branch3x3_1 = conv2d_factory(x, 64, (1, 1), is_training=is_training)
-        branch3x3_1 = conv2d_factory(branch3x3_1, 64, (3, 1), is_training=is_training)
-        branch3x3_1 = conv2d_factory(branch3x3_1, 64, (1, 3), is_training=is_training)
+        branch5x5 = conv2d_factory(x, 128, (1, 1), is_training=is_training)
+        branch5x5 = conv2d_factory(branch5x5, 128, (5, 1), is_training=is_training)
+        branch5x5 = conv2d_factory(branch5x5, 128, (1, 5), is_training=is_training)
 
-        branch3x3_2 = conv2d_factory(x, 64, (1, 1), is_training=is_training)
-        branch3x3_2 = conv2d_factory(branch3x3_2, 64, (3, 3), is_training=is_training)
-        branch3x3_2 = conv2d_factory(branch3x3_2, 64, (3, 1), is_training=is_training)
-        branch3x3_2 = conv2d_factory(branch3x3_2, 64, (1, 3), is_training=is_training)
+        branch3x3_2 = conv2d_factory(x, 128, (1, 1), is_training=is_training)
+        branch3x3_2 = conv2d_factory(branch3x3_2, 128, (3, 3), is_training=is_training)
+        branch3x3_2 = conv2d_factory(branch3x3_2, 128, (3, 1), is_training=is_training)
+        branch3x3_2 = conv2d_factory(branch3x3_2, 128, (1, 3), is_training=is_training)
 
         branch_pool = AveragePooling2D(pool_size=(2, 2), strides=(1, 1), padding='same')(x)
-        branch_pool = conv2d_factory(branch_pool, 64, (1, 1), is_training=is_training)
+        branch_pool = conv2d_factory(branch_pool, 128, (1, 1), is_training=is_training)
 
-        x = concatenate([branch1x1, branch3x3_1, branch3x3_2, branch_pool], axis=-1)
+        x = concatenate([branch1x1, branch5x5, branch3x3_2, branch_pool], axis=-1)
 
     x = GlobalAveragePooling2D()(x)
     x = drop_connect_dense(x, nb_classes, activation='softmax',
@@ -91,3 +91,6 @@ def inception_v3(nb_classes, is_training=True):
 
     model = Model(inputs=model_input, outputs=x)
     return model
+
+model = inception_v3(10, is_training=True)
+model.summary()
